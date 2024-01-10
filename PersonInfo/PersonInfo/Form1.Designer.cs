2da2448f2fa286fbc963fc2f1f4e47b3008d1fa2
Mario Ganev <82400980+marioganev@users.noreply.github.com>
2024-01-10 12:07:02 +0200

    Add files via upload

---
+++ PersonInfo/PersonInfo/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace PersonInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.FirstName = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.Label();
+            this.ModifyData = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Приеми";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Location = new System.Drawing.Point(44, 50);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(29, 13);
+            this.FirstName.TabIndex = 1;
+            this.FirstName.Text = "Име";
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSize = true;
+            this.LastName.Location = new System.Drawing.Point(44, 101);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(56, 13);
+            this.LastName.TabIndex = 2;
+            this.LastName.Text = "Фамилия";
+            // 
+            // ModifyData
+            // 
+            this.ModifyData.AutoSize = true;
+            this.ModifyData.Location = new System.Drawing.Point(44, 155);
+            this.ModifyData.Name = "ModifyData";
+            this.ModifyData.Size = new System.Drawing.Size(33, 13);
+            this.ModifyData.TabIndex = 3;
+            this.ModifyData.Text = "Дата";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(135, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(135, 152);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 299);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.ModifyData);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Label LastName;
+        private System.Windows.Forms.Label ModifyData;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ PersonInfo/PersonInfo/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=LENOVO6;Initial Catalog=PersonInfoNew;Integrated Security=True"))
+            {
+                string sqlQuery = "insert into dbo.PersonInfo (FirstName, LastName, ModifiedDate) values(@FirstName, @LastName, @ModifiedDate)";
+ using (SqlCommand cmd = new SqlCommand(sqlQuery, con))
+                {
+
+                    cmd.Parameters.AddWithValue("@FirstName", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@LastName", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@ModifiedDate", textBox4.Text);
+                    con.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    con.Close();
+                    if (i != 0)
+                    {
+                        MessageBox.Show(i + "Data Saved");
+                    }
+                }
+            }
+        }
+
+    }
+}
